16f4e90147b819ba97ee2717b7bee35118b0016d
qweeedly <rerere>
2021-01-22 13:22:48 +0500

    Initial commit

---
+++ app/src/main/java/com/example/konvertor/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.konvertor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ResultClick(View view) {
+    }
+}
